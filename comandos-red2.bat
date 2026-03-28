a74b4485d837859d503adc7b66713487186b7604
EliBS01 <eligabrielprobrawl@gmail.com>
2026-03-27 23:15:27 -0400

    Add files via upload

---
+++ comandos-red2.bat
@@ -0,0 +1,15 @@
+ipconfig
+ipconfig /all
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+ping (ip)
+tracert (ip)
+nslookup (dominio)
+arp -a
+netstat -a
+netstat -b
+netstat -abno
+netsh
+netsh wlan show profiles 
+netsh wlan show profile (nombre_perfil_red) key=clear
